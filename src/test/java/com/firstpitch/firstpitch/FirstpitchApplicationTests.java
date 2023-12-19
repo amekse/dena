0808435ada1987d1ccafcc0b498dd11f552fb745
Saswata Das <dassaswata1996@gmail.com>
2023-12-19 20:12:06 +0530

    init

---
+++ src/test/java/com/firstpitch/firstpitch/FirstpitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstpitch.firstpitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstpitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
